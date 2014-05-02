d4ab6de5ac9a9c2d823ca46672709e6f954be6c4
Chris McClymont <chris@mcclymont.it>
2014-05-02 12:54:39 +1000

    setup.py license, mantainer, description, zip_safe

--- setup.py
+++ setup.py
@@ -14,14 +14,17 @@ if os.path.isdir(man_dir):
 
 setup(name='ovm_ctl',
     version=__version__,
+    license='BSD Simplified 2-clause licence',
+    description='Python bindings and command line utility for the OrionVM IaaS platform',
     long_description=open('README').read(),
     url='https://github.com/orionvm/ovm-ctl',
+    maintainer='Chris McClymont',
+    maintainer_email='chris.mcclymont@orionvm.com',
     packages=['ovm_ctl'],
     scripts=['ovm_ctl/ovm-ctl'],
     package_data={
         'ovm_ctl': [ 'version', ],
     },
-    install_requires=[
-    ],
-    data_files=data_files
+    data_files=data_files,
+    zip_safe=False
 )
